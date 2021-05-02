0cfb0caa35e42b1fc224ef642a303eded2129d90
gomesmauricio <josemauricio.gomes@gmail.com>
2021-05-01 23:54:34 -0300

    PUT /colaboradores

--- backend/src/main/java/desafiomv/desafiomv/controllers/ColaboradorController.java
+++ backend/src/main/java/desafiomv/desafiomv/controllers/ColaboradorController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -38,6 +40,13 @@ public class ColaboradorController {
 		return service.insertColaborador(colaborador);
 		
 	}
+	
+	@PutMapping("/{cpf}")
+	public ResponseEntity<MessageResponseDTO> alterar(@PathVariable String cpf){
+		MessageResponseDTO dto = service.alterar(cpf);
+		return ResponseEntity.ok().body(dto);
+	}
+	
 	
 	
 }
--- backend/src/main/java/desafiomv/desafiomv/services/ColaboradorService.java
+++ backend/src/main/java/desafiomv/desafiomv/services/ColaboradorService.java
@@ -1,9 +1,11 @@
 package desafiomv.desafiomv.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.web.savedrequest.SavedCookie;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,6 +26,7 @@ public class ColaboradorService {
 		return list.stream().map(x -> new ColaboradorDTO(x)).collect(Collectors.toList());
 	}
 	
+	@Transactional
 	public MessageResponseDTO insertColaborador(Colaborador colaborador) {
 		Colaborador savedColaborador = repository.save(colaborador);
 		return MessageResponseDTO
@@ -32,5 +35,20 @@ public class ColaboradorService {
 				.build();
 		
 	}
+	
+	@Transactional
+	public MessageResponseDTO alterar(String cpf) {
+		Colaborador colaborador = repository.getOne(cpf);
+		colaborador.setCpf(colaborador.getCpf());
+		colaborador.setNome(colaborador.getNome());
+		colaborador.setOpcao(colaborador.getOpcao());
+		colaborador = repository.save(colaborador);
+		return MessageResponseDTO
+				.builder()
+				.message("Colaborador: " + colaborador.getNome()+ " Alterado com sucesso")
+				.build();
+	}
+
+
 
 }
